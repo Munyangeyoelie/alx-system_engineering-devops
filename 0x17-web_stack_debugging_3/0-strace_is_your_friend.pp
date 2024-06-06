ab53bc171e3894a36638fa9b666cb1cfe89a3f53
Munyangeyo Elie <munyangeyoelie38@gmail.com>
2024-06-05 18:12:32 -0700

    changes

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,6 @@
-# A puppet manuscript to replace a line in a file on a server
+# automated puppet fix (to find out why Apache is returning a 500 error)
 
-$file_to_edit = '/var/www/html/wp-settings.php'
-
-#replace line containing "phpp" with "php"
-
-exec { 'replace_line':
-  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
-  path    => ['/bin','/usr/bin']
+exec { 'Fix wordpress site':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
